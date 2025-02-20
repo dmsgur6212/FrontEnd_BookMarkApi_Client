0ac45ea6b40f1658e77b94bd5b81bb2abb61bb8f
dmsgur6212 <dmsgur6212@naver.com>
2025-02-20 16:41:52 +0900

    pagination added

--- src/app/components/Bookmarks.tsx
+++ src/app/components/Bookmarks.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import { BookmarkType } from '@/types';
-import Bookmark from './Bookmark';
+import { BookmarksResponse } from '@/types';
+import Bookmark from '@/app/components/Bookmark';
+import Pagination from '@/app/components/Pagination';
 
 type Props = {
-    bookmarks: BookmarkType[]
+    bookmarks: BookmarksResponse
 }
 
 const Bookmarks: React.FC<Props> = ({bookmarks}) => {
     return (
         <div>
-            <h1>Welcome to Bookmarker</h1>
-            {
-            bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark} />)
-            }            
+            <Pagination bookmarks={bookmarks} />
+            { bookmarks.data.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark} />) }            
         </div>
     );
 };
