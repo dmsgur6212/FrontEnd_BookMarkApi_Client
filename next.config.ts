bbc9dc767cf9ff0a6a580272825731fa2af0b264
dmsgur6212 <dmsgur6212@naver.com>
2025-02-21 10:23:02 +0900

    done

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,21 @@ const nextConfig = {
         permanent: true
       }
     ]
-  }
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `http://bookmarker-api-svc:8080/api/:path*`//'http://bookmarker-api-svc:8080/:path*/' // Proxy to Backend
+      }
+    ]
+  },
+  serverRuntimeConfig: {
+    API_BASE_URL: process.env.NEXT_PUBLIC_SERVER_API_BASE_URL
+  },
+  publicRuntimeConfig: {
+    API_BASE_URL: process.env.NEXT_PUBLIC_CLIENT_API_BASE_URL
+  },
 };
 
 export default nextConfig;
--- src/lib/bookmarkApi.ts
+++ src/lib/bookmarkApi.ts
@@ -1,15 +1,15 @@
 import { BookmarksResponse } from "@/types"
-import axios, { AxiosResponse } from "axios"
+import axios, { AxiosResponse } from "axios";
 
-const API_BASE_URL = 'http://localhost:8080'
+const API_BASE_URL = process.env.NEXT_PUBLIC_SERVER_API_BASE_URL
 
 export async function getAllBookmarks(page: number, query: string): Promise<BookmarksResponse> {
-    
+        
     let API_URL = `${API_BASE_URL}/api/bookmarks?page=${page}`
     if(query) {
         API_URL += `&query=${query}`
     }
-    console.log(`url = ${API_URL}`)
+    console.log(`>> url = ${API_URL}`)
     
     const res = await axios.get<BookmarksResponse>(API_URL)
 
@@ -17,6 +17,7 @@ export async function getAllBookmarks(page: number, query: string): Promise<Book
 }
 
 export async function saveBookmark(bookmark:{title: string, url: string}) {
-    const res: AxiosResponse<unknown, unknown> = await axios.post(`${API_BASE_URL}/api/bookmarks`, bookmark)
+    const res: AxiosResponse<unknown, unknown> = await axios.post(`/api/bookmarks`, bookmark)
+    // const res = await axios.post(`${getApiUrl()}/api/bookmarks`, bookmark)
     return res.data
 }
